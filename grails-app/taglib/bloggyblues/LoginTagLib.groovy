9d51995d3dd1c8fd931d0de212b368de840812a3
Justin Schoen <j23schoen@gmail.com>
2016-07-05 16:45:49 -0400

    final friday push

--- grails-app/taglib/bloggyblues/LoginTagLib.groovy
+++ grails-app/taglib/bloggyblues/LoginTagLib.groovy
@@ -4,9 +4,9 @@ class LoginTagLib {
   def loginControl = {
     if(session.user){
       out << "Hello, ${session.user.name}"
-      out << """[${link(action:"logout", controller:"user"){"Logout"}}]"""
+      out << """${link(action:"logout", controller:"user", class:"btn btn-primary"){"Logout"}}"""
     } else {
-      out << """[${link(action:"login", controller:"user"){"Login"}}]"""      
+      out << """${link(action:"login", controller:"user", class:"btn btn-primary"){"Login"}}"""      
     }
   }
 }
